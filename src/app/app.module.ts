a3944cc0ccbe75ab6a08bde04330d3549517e670
Oscar Söderlund <oscar@nebulr.group>
2022-02-11 13:31:55 +0100

    Manual editing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,24 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import {
+  NblocksIonicModule,
+  AUTH_HTTP_INTERCEPTOR_PROVIDERS,
+} from '@nebulr-group/nblocks-ionic';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    NblocksIonicModule.forRoot({}),
+  ],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    AUTH_HTTP_INTERCEPTOR_PROVIDERS,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
